5733648646ef013c9a0b4073928f50cc3f34b4f4
Maksym Shelestiuk <Maksym_Shelestiuk@epam.com>
2023-05-09 23:08:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLIPSExpertSystem;
+
+namespace CLIPSConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpertResponse expertResponse = new ExpertResponse();
+
+            Console.WriteLine("BMI " + expertResponse.GetBMIValue());
+
+            Console.WriteLine("BMR " + expertResponse.GetBMRValue());
+
+            Console.WriteLine("Formal " + expertResponse.GetBMIFormal());
+
+            Console.WriteLine("Exercises:");
+            foreach (var exercise in expertResponse.GetExercises())
+            {
+                Console.WriteLine(exercise);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
